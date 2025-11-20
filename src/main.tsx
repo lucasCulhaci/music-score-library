25cb030427312e4af663289aeb91b9111c9ca15a
lucasCulhaci <40703191+lucasCulhaci@users.noreply.github.com>
2025-11-20 17:04:40 +0100

    Created the necessary files and added all necessary dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
+import Scores from './pages/Scores.tsx'
+import DetailsScore from './pages/DetailsScore.tsx'
+
+
+const browserRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Scores/>
+  },
+  {
+    path: 'details/:scoreID',
+    element: <DetailsScore/>
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={browserRouter}/>
   </StrictMode>,
 )
